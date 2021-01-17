d309460d46aed833baf945ae15d13950dc99b270
narges <hasheminarges76@gmail.com>
2021-01-17 14:54:25 +0330

    add premissions and authorization and comments

--- articles/admin.py
+++ articles/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-
 # Register your models here.
-from .models import Article
+from .models import Article, Comment
 
-admin.site.register(Article)
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
--- articles/views.py
+++ articles/views.py
@@ -2,29 +2,47 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView,CreateView,DetailView
 from django.views.generic.edit import UpdateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Article
 # Create your views here.
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin,ListView):
     model = Article
     template_name = 'article_list.html'
     context_object_name = 'all_articles'
+    login_url = 'login'
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin,CreateView):
     model = Article
     template_name = 'article_new.html'
-    fields = ('title', 'body', 'author',)
+    fields = ('title', 'body',)
+    login_url = 'login'
 
-class ArticleDetailView(DetailView):
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class ArticleDetailView(LoginRequiredMixin,DetailView):
     model = Article
     template_name = 'article_detail.html'
+    login_url = 'login'
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(UserPassesTestMixin,LoginRequiredMixin,DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+    login_url = 'login'
 
-class ArticleUpdateView(UpdateView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleUpdateView(UserPassesTestMixin,LoginRequiredMixin,UpdateView):
     model = Article
     template_name = 'article_edit.html'
     fields = ('title', 'body',)
+    login_url = 'login'
+    
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
